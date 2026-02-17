e76a58903a4362cd0c986a0dc5bdd90c60c109fd
ScopeRicrit <wahyudiwijaya2309@gmail.com>
2026-02-18 00:17:49 +0800

    Mengupload ketiga file Top Bar

---
+++ templates/top_bar/top_bar.php
@@ -0,0 +1,86 @@
+<?php
+  // Top Bar / Header
+  // Bilah navigasi di bagian paling atas halaman web.
+  function top_bar_html($title = "Default Title", $desc = "Default Description", $img = "/images/fallback.jpg") {
+    // Heredoc String merupakan sejenis sintaks string yang menetapkan warna kode html di VS Code.
+    // Ia diketik dengan tiga tanda panah kiri dan nama bebas.
+    // Seperti string double quoted (""), ia dapat disisipkan variabel untuk fleksibilitas.
+    $html = <<<HTML
+      <header>
+        <nav id="root_nav">
+          <nav id="primary_nav">  
+            <a href="home">
+              <img id="logo" src="../../assets/images/logo_lms.png" alt="logo-scis.png">
+            </a>
+            <a href="home">Beranda</a>
+            <a href="competitions">Perlombaan</a>
+            <a href="students">Siswa</a>
+          </nav>
+
+          <nav id="user_menu">
+            <span id="signed_account">
+              <a id="profile_pic" href="profile">
+                <img src="../../assets/images/default_pfp.jpg" alt="default_pfp.jpg">
+              </a>
+              <p id="username">Anonymous</p>
+            </span>
+            <a href="login">Login</a>
+          </nav>
+        </nav>
+      </header>
+    HTML;
+
+    echo $html;
+  }
+
+  // Elemen <style> harus dicetak di dalam elemen <head> untuk bisa bekerja.
+  function top_bar_css() {
+    $html = <<<HTML
+      <style>
+        header * {
+          margin: auto 0;
+        }
+        
+        header {
+          background-color: white;
+          margin: 0;
+        }
+
+        header nav, header span {
+          display: flex;
+        }
+        
+        header #root_nav {
+          justify-content: space-between;
+          padding: 0 20px;
+        }
+
+        header #primary_nav {
+          gap: 30px;
+        }
+        
+        header #user_menu {
+          gap: 10px;
+        }
+
+        header span {
+          gap: 10px;
+        }
+
+        header #login_button {
+          padding: 8px 16px;
+        }
+
+        header #logo {
+          height: 50px;
+        }
+
+        header #profile_pic img {
+          height: 30px;
+        }
+      </style>
+    HTML;
+
+    echo $html;
+  }
+?>
