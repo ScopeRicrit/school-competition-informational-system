88b6d2e68930296c3a080ac7dc865be138005605
ScopeRicrit <wahyudiwijaya2309@gmail.com>
2026-02-21 23:09:43 +0800

    Mengupdate top_bar.php
    - Menghubungi halaman home dan comps
    - Menerapkan parameter dinamis untuk foto profil pengguna.

--- templates/top_bar/top_bar.php
+++ templates/top_bar/top_bar.php
@@ -1,7 +1,9 @@
 <?php
   // Top Bar / Header
   // Bilah navigasi di bagian paling atas halaman web.
-  function top_bar_html($title = "Default Title", $desc = "Default Description", $img = "/images/fallback.jpg") {
+  function top_bar_html(
+    $profile_pic = "../../assets/images/default_pfp.jpg",
+  ) {  
     // Heredoc String merupakan sejenis sintaks string yang menetapkan warna kode html di VS Code.
     // Ia diketik dengan tiga tanda panah kiri dan nama bebas.
     // Seperti string double quoted (""), ia dapat disisipkan variabel untuk fleksibilitas.
@@ -12,17 +14,16 @@
             <a href="home">
               <img id="logo" src="../../assets/images/logo_lms.png" alt="logo-scis.png">
             </a>
-            <a href="home">Beranda</a>
-            <a href="competitions">Perlombaan</a>
+            <a href="../../pages/home/home.php">Beranda</a>
+            <a href="../../pages/comp/comps.php">Perlombaan</a>
             <a href="students">Siswa</a>
           </nav>
 
           <nav id="user_menu">
             <span id="signed_account">
               <a id="profile_pic" href="profile">
-                <img src="../../assets/images/default_pfp.jpg" alt="default_pfp.jpg">
+                <img src="$profile_pic" alt="pfp.jpg">
               </a>
-              <p id="username">Anonymous</p>
             </span>
             <a href="login">Login</a>
           </nav>
